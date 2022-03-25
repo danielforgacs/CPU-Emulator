2644c7ea5ee8d7bc94c9dd2b950a4816dd2e9335
df@ <@>
2022-03-25 20:04:23 +0000

    started CPU

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct CPU {
+    // opcode: operation number and operands as registers.
+    current_operation: u16,
+    registers: [u8; 2],
+}
+
+impl CPU {
+    fn new() -> Self {
+        Self {
+            current_operation: 0,
+            registers: [0; 2],
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
 }
