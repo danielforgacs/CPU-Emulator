382b29f6cda35bc7c354a7e74db90974d7be99fb
df@ <@>
2022-03-26 00:59:01 +0000

    ok

--- src/main.rs
+++ src/main.rs
@@ -6,55 +6,77 @@ https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 */
 
 struct CPU {
-    // opcode: operation number and operands as registers.
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    // Normally referred to as "program counter"
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn new() -> Self {
         Self {
-            // Initialisation with a No-Op - do nothing.
-            current_operation: 0,
-            registers: [0; 2],
+            registers: [0; 16],
+            position_in_memory: 0,
+            memory: [0; 4096],
         }
     }
 
     fn read_opcode(&mut self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+        op_byte1 << 8 | op_byte2
     }
 
     fn run(&mut self) {
-        let opcode = self.read_opcode();
+        loop {
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
 
-        // decoding opcode
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
+            // decoding opcode
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-            (8, _, _, 4) => self.add(x, y),
-            _ => panic!("opcode not implemented: {:x}", opcode),
-        };
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => { return; },
+                (8, _, _, 4) => self.add(x, y),
+                _ => panic!("opcode not implemented: {:x}", opcode),
+            };
+        }
     }
 
     fn add(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
     }
 }
 
 fn main() {
     let mut cpu = CPU::new();
-    // 8 signifies that the operation involves two registers.
-    // 0 maps to cpu.registers[0].
-    // 1 maps to cpu.registers[1].
-    // 4 indicates addition.
-    cpu.current_operation = 0x8014;
+
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    let mem = &mut cpu.memory;
+
+    mem[0] = 0x80; mem[1] = 0x14;
+    mem[2] = 0x80; mem[3] = 0x24;
+    mem[4] = 0x80; mem[5] = 0x34;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 15);
+    assert_eq!(cpu.registers[0], 35);
 }
