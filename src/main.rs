d8111387115b4a4980546cafe00b21f67ee7dbe5
df@ <@>
2022-03-26 00:23:03 +0000

    OK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+https://en.wikipedia.org/wiki/CHIP-8
+https://chip-8.github.io/links/
+https://github.com/mattmikolay/chip-8/wiki/CHIP%E2%80%908-Instruction-Set
+https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+*/
+
 struct CPU {
     // opcode: operation number and operands as registers.
     current_operation: u16,
@@ -12,6 +19,28 @@ impl CPU {
             registers: [0; 2],
         }
     }
+
+    fn read_opcode(&mut self) -> u16 {
+        self.current_operation
+    }
+
+    fn run(&mut self) {
+        let opcode = self.read_opcode();
+
+        let c = ((opcode & 0xF000) >> 12) as u8;
+        let x = ((opcode & 0x0F00) >> 8) as u8;
+        let y = ((opcode & 0x00F0) >> 4) as u8;
+        let d = ((opcode & 0x000F) >> 0) as u8;
+
+        match (c, x, y, d) {
+            (8, _, _, 4) => self.add(x, y),
+            _ => panic!("opcode not implemented: {:x}", opcode),
+        };
+    }
+
+    fn add(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] += self.registers[y as usize];
+    }
 }
 
 fn main() {
@@ -23,4 +52,8 @@ fn main() {
     cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 15);
 }
